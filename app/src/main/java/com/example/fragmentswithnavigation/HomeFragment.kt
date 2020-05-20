82c00890d4fa5b92c821eca83996cc25dc688d3f
Zhu <mrzhugang1993@163.com>
2020-05-20 17:11:52 +0800

    Switch Fragments between HomeFragment and SecondFragment

---
+++ app/src/main/java/com/example/fragmentswithnavigation/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.fragmentswithnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        view?.findViewById<Button>(R.id.home_btn)?.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
+    }
+}
